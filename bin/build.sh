4f8dd4a905da6062fa6048b45e4eb6687ccbb120
aminurislamarnob <eng.aminurislam@gmail.com>
2025-08-30 19:18:46 +0600

    Init new plugin structure

---
+++ bin/build.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# Exit if any command fails.
+set -e
+
+# Change to the expected directory.
+cd "$(dirname "$0")"
+cd ..
+DIR=$(pwd)
+BUILD_DIR="$DIR/build/wp-login-logout-redirect"
+
+# Enable nicer messaging for build status.
+BLUE_BOLD='\033[1;34m'
+GREEN_BOLD='\033[1;32m'
+RED_BOLD='\033[1;31m'
+YELLOW_BOLD='\033[1;33m'
+COLOR_RESET='\033[0m'
+
+error() {
+    echo -e "\n${RED_BOLD}$1${COLOR_RESET}\n"
+}
+status() {
+    echo -e "\n${BLUE_BOLD}$1${COLOR_RESET}\n"
+}
+success() {
+    echo -e "\n${GREEN_BOLD}$1${COLOR_RESET}\n"
+}
+warning() {
+    echo -e "\n${YELLOW_BOLD}$1${COLOR_RESET}\n"
+}
+
+status "💃 Time to build the Wp Login Logout Redirect ZIP file 🕺"
+
+# remove the build directory if exists and create one
+rm -rf "$DIR/build"
+mkdir -p "$BUILD_DIR"
+
+# Run the build.
+# status "Installing dependencies... 📦"
+# npm install
+
+status "Generating build... 👷‍♀️"
+
+# Copy all files
+status "Copying files... ✌️"
+FILES=(wp-login-logout-redirect.php readme.txt dist includes templates assets languages composer.json composer.lock)
+
+for file in ${FILES[@]}; do
+    if [ -f "$file" ] || [ -d "$file" ]; then
+        cp -R $file $BUILD_DIR
+    fi
+done
+
+# Install composer dependencies
+status "Installing dependencies... 📦"
+cd $BUILD_DIR
+composer install --optimize-autoloader --no-dev -q
+
+# Remove composer files
+rm composer.json composer.lock
+
+# go one up, to the build dir
+status "Creating archive... 🎁"
+cd ..
+zip -r -q wp-login-logout-redirect.zip wp-login-logout-redirect
+
+# remove the source directory
+rm -rf wp-login-logout-redirect
+
+success "Done. You've built Wp Login Logout Redirect! 🎉 "
+echo -e "\n${BLUE_BOLD}File Path${COLOR_RESET}: ${YELLOW_BOLD}$(pwd)/wp-login-logout-redirect.zip${COLOR_RESET} \n"
---
+++ includes/WpLoginLogoutRedirect.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace PluginizeLab\WpLoginLogoutRedirect;
+
+/**
+ * WpLoginLogoutRedirect class
+ *
+ * @class WpLoginLogoutRedirect The class that holds the entire WpLoginLogoutRedirect plugin
+ */
+final class WpLoginLogoutRedirect {
+
+    /**
+     * Plugin version
+     *
+     * @var string
+     */
+    public $version = '0.0.1';
+
+    /**
+     * Instance of self
+     *
+     * @var WpLoginLogoutRedirect
+     */
+    private static $instance = null;
+
+    /**
+     * Holds various class instances
+     *
+     * @since 2.6.10
+     *
+     * @var array
+     */
+    private $container = [];
+
+    /**
+     * Constructor for the WpLoginLogoutRedirect class
+     *
+     * Sets up all the appropriate hooks and actions
+     * within our plugin.
+     */
+    private function __construct() {
+        $this->define_constants();
+
+        register_activation_hook( WP_LOGIN_LOGOUT_REDIRECT_FILE, [ $this, 'activate' ] );
+        register_deactivation_hook( WP_LOGIN_LOGOUT_REDIRECT_FILE, [ $this, 'deactivate' ] );
+
+        add_action( 'plugins_loaded', [ $this, 'init_plugin' ] );
+        add_action( 'woocommerce_flush_rewrite_rules', [ $this, 'flush_rewrite_rules' ] );
+    }
+
+    /**
+     * Initializes the WpLoginLogoutRedirect() class
+     *
+     * Checks for an existing WpLoginLogoutRedirect instance
+     * and if it doesn't find one then create a new one.
+     *
+     * @return WpLoginLogoutRedirect
+     */
+    public static function init() {
+        if ( self::$instance === null ) {
+			self::$instance = new self();
+		}
+
+        return self::$instance;
+    }
+
+    /**
+     * Magic getter to bypass referencing objects
+     *
+     * @since 2.6.10
+     *
+     * @param string $prop
+     *
+     * @return Class Instance
+     */
+    public function __get( $prop ) {
+		if ( array_key_exists( $prop, $this->container ) ) {
+            return $this->container[ $prop ];
+		}
+    }
+
+    /**
+     * Placeholder for activation function
+     *
+     * Nothing is being called here yet.
+     */
+    public function activate() {
+        // Rewrite rules during wp_login_logout_redirect activation
+        if ( $this->has_woocommerce() ) {
+            $this->flush_rewrite_rules();
+        }
+    }
+
+    /**
+     * Flush rewrite rules after wp_login_logout_redirect is activated or woocommerce is activated
+     *
+     * @since 3.2.8
+     */
+    public function flush_rewrite_rules() {
+        // fix rewrite rules
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Placeholder for deactivation function
+     *
+     * Nothing being called here yet.
+     */
+    public function deactivate() {     }
+
+    /**
+     * Define all constants
+     *
+     * @return void
+     */
+    public function define_constants() {
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_VERSION' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_VERSION', $this->version );
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_DIR' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_DIR', dirname( WP_LOGIN_LOGOUT_REDIRECT_FILE ) );
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_INC_DIR' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_INC_DIR', WP_LOGIN_LOGOUT_REDIRECT_DIR . '/includes' );
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_TEMPLATE_DIR' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_TEMPLATE_DIR', WP_LOGIN_LOGOUT_REDIRECT_DIR . '/templates' );
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_ASSET' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_ASSET', plugins_url( 'assets', WP_LOGIN_LOGOUT_REDIRECT_FILE ) );
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_ADMIN_ASSET' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_ADMIN_ASSET' , WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_ASSET . '/admin' );
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_PUBLIC_ASSET' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_PUBLIC_ASSET' , WP_LOGIN_LOGOUT_REDIRECT_PLUGIN_ASSET . '/public' );
+
+        // give a way to turn off loading styles and scripts from parent theme
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_LOAD_STYLE' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_LOAD_STYLE', true );
+        defined( 'WP_LOGIN_LOGOUT_REDIRECT_LOAD_SCRIPTS' ) || define( 'WP_LOGIN_LOGOUT_REDIRECT_LOAD_SCRIPTS', true );
+    }
+
+    /**
+     * Load the plugin after WP User Frontend is loaded
+     *
+     * @return void
+     */
+    public function init_plugin() {
+        $this->includes();
+        $this->init_hooks();
+
+        do_action( 'wp_login_logout_redirect_loaded' );
+    }
+
+    /**
+     * Initialize the actions
+     *
+     * @return void
+     */
+    public function init_hooks() {
+        // initialize the classes
+        add_action( 'init', [ $this, 'init_classes' ], 4 );
+        add_action( 'plugins_loaded', [ $this, 'after_plugins_loaded' ] );
+    }
+
+    /**
+     * Include all the required files
+     *
+     * @return void
+     */
+    public function includes() {
+        // include_once STUB_PLUGIN_DIR . '/functions.php';
+    }
+
+    /**
+     * Init all the classes
+     *
+     * @return void
+     */
+    public function init_classes() {
+        $this->container['scripts'] = new Assets();
+    }
+
+    /**
+     * Executed after all plugins are loaded
+     *
+     * At this point wp_login_logout_redirect Pro is loaded
+     *
+     * @since 2.8.7
+     *
+     * @return void
+     */
+    public function after_plugins_loaded() {
+        // Initiate background processes and other tasks
+    }
+
+    /**
+     * Check whether woocommerce is installed and active
+     *
+     * @since 2.9.16
+     *
+     * @return bool
+     */
+    public function has_woocommerce() {
+        return class_exists( 'WooCommerce' );
+    }
+
+    /**
+     * Check whether woocommerce is installed
+     *
+     * @since 3.2.8
+     *
+     * @return bool
+     */
+    public function is_woocommerce_installed() {
+        return in_array( 'woocommerce/woocommerce.php', array_keys( get_plugins() ), true );
+    }
+
+    /**
+	 * Get the plugin url.
+	 *
+	 * @return string
+	 */
+	public function plugin_url() {
+		return untrailingslashit( plugins_url( '/', WP_LOGIN_LOGOUT_REDIRECT_FILE ) );
+	}
+
+    /**
+     * Get the template file path to require or include.
+     *
+     * @param string $name
+     * @return string
+     */
+    public function get_template( $name ) {
+        $template = untrailingslashit( WP_LOGIN_LOGOUT_REDIRECT_TEMPLATE_DIR ) . '/' . untrailingslashit( $name );
+
+        return apply_filters( 'wp-login-logout-redirect_template', $template, $name );
+    }
+}
