21edc600359f07533c6e7d2ab80aa3631506019a
aminurislamarnob <eng.aminurislam@gmail.com>
2025-08-30 21:37:29 +0600

    Added vendor folder to git

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginizelab/wp-login-logout-redirect',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '4099bb116c99601f2deb174f5670ce6939b1c193',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pluginizelab/wp-login-logout-redirect' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '4099bb116c99601f2deb174f5670ce6939b1c193',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
