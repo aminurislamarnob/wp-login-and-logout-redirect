0ad718c26fa971d083ab67279a0c5913b97fb5eb
aminurislamarnob <eng.aminurislam@gmail.com>
2025-08-30 21:04:44 +0600

    Update build.sh for missing plugin main file during build

--- bin/build.sh
+++ bin/build.sh
@@ -43,7 +43,7 @@ status "Generating build... 👷‍♀️"
 
 # Copy all files
 status "Copying files... ✌️"
-FILES=(wp-login-and-logout-redirect.php readme.txt dist includes templates assets languages composer.json composer.lock)
+FILES=(wp-login-logout-redirect.php readme.txt dist includes templates assets languages composer.json composer.lock)
 
 for file in ${FILES[@]}; do
     if [ -f "$file" ] || [ -d "$file" ]; then
